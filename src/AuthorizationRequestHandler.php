8c4f598b0ac2f0e08546ea07f9e1d789877d0edb
xiaohui.lam <xiaohui.lam@icloud.com>
2019-10-22 10:29:10 +0800

    fixbug

--- src/AuthorizationRequestHandler.php
+++ src/AuthorizationRequestHandler.php
@@ -1,8 +1,13 @@
 <?php
+
 namespace Ikcrm;
 
-use function GuzzleHttp\json_decode;
+use Carbon\Carbon;
+use Psr\Http\Message\RequestInterface;
+use GuzzleHttp\Client;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\json_decode;
 
 class AuthorizationRequestHandler
 {
@@ -15,38 +20,89 @@ class AuthorizationRequestHandler
     /**
      * @var Weiwenjia
      */
-    protected $weiwenjia = null;
-
-    public function __construct(array $config = [], Ikcrm $weiwenjia)
+    protected $weiwenjia;
+    public function __construct(Ikcrm $weiwenjia, $login = null, $password = null, $corp_id = null)
     {
-        parent::__construct($config);
         $this->weiwenjia = $weiwenjia;
+        $this->login = $login;
+        $this->password = $password;
+        $this->corp_id = config('ikcrm.corp_id', $corp_id);
     }
-
-    public function request($method, $uri = '', array $options = [], $retry = 3)
+    public function _login($login = null, $password = null, $corp_id = null)
     {
-        _retry:
-        $request = parent::request($method, $uri, $options);
-        $body = $request->getBody();
-        $json = json_decode($body, true);
-        if (!isset($json['code'])) {
-            throw new HttpException(500, 'Response json with no code');
+        if ($login) {
+            $this->login = $login;
         }
-        if (100000 == $json['code'] || 100401 == $json['code'] || 100400 == $json['code']) {
-            $retry--;
-            if ($retry < 0) {
-                throw new HttpException(401, 'Auth fail after retried 3 times, and final caught:' . $json['code'] . ' - ' . $json['message']);
-            }
-            if (function_exists('cache')) {
-                cache()->forget('IKCRM_TOKEN_' . $this->weiwenjia->authorization_handler->getLogin());
-            }
-            $this->weiwenjia->authorization_handler->_login();
-            goto _retry;
+        if ($password) {
+            $this->password = $password;
         }
+        if ($corp_id) {
+            $this->corp_id = $this->corp_id;
+        }
+        if (function_exists('cache')) {
+            $this->token = cache()->get('IKCRM_TOKEN_' . $this->login);
+            if ($this->token) {
+                return;
+            }
+        }
+        $data  = [
+            'device' => $this->device,
+            'version_code' => $this->version_code,
+            'login' => $this->login,
+            'password' => $this->password,
+            'corp_id' => $this->corp_id,
+        ];
+        $client = new Client(['base_uri' => $this->weiwenjia->domain,]);
+        $login = $client->post(Ikcrm::API_LOGIN, ['form_params' => $data,]);
+        $json = json_decode($login->getBody(), true);
         if (0 != $json['code']) {
-            throw new HttpException($json['code'], $json['message']);
+            throw new HttpException(401, $json['message'], null, [], $json['code']);
         }
-
-        return $json['data'];
+        $this->token = $json['data']['user_token'];
+        if (function_exists('cache')) {
+            cache()->put('IKCRM_TOKEN_' . $this->login, $this->token, 60);
+        }
+    }
+    public function getToken()
+    {
+        if (!$this->token) {
+            $this->_login();
+        }
+        return $this->token;
+    }
+    public function getLogin()
+    {
+        return $this->login;
+    }
+    /**
+     * 触发
+     *
+     * @param \callable $handler
+     * @return \callable
+     */
+    public function __invoke($handler, $retry = 3)
+    {
+        return function (RequestInterface $request, array $options) use ($handler, $retry) {
+            $headerValue = 'Token token=%s, device=%s, version_code=%s';
+            $request = $request->withAddedHeader(
+                'Authorization',
+                sprintf($headerValue, $this->token, $this->device, $this->version_code)
+            );
+            /**
+             * @var \GuzzleHttp\Promise\FulfilledPromise $response
+             */
+            $response = $handler($request, $options);
+            return $response->then(function ($response) use ($handler, $retry) {
+                /**
+                 * @var ResponseInterface $response
+                 */
+                $body = $response->getBody();
+                $json = json_decode($body, true);
+                if (!isset($json['code'])) {
+                    throw new HttpException(500, 'Response json with no code');
+                }
+                return $response;
+            });
+        };
     }
 }
